4df3d66ff6c7b16304ca18919a2cc109be170b6b
Aditya Rai <adityarai@Adityas-MacBook-Pro.local>
2020-10-10 13:44:10 +0530

    fixing navbar

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,30 +1,7 @@
 AOS.init();
 
-
-
-if($(window).innerWidth > 768){
-  $('body').css('padding-top', $('.navbar').outerHeight()+180 + 'px')
-}
-
-else{
-    $('body').css('padding-top', $('.navbar').outerHeight()+35 + 'px')
-}
-
-// detect scroll top or down
-if ($('.smart-scroll').length > 0) { // check if element exists
-    var last_scroll_top = 0;
-    $(window).on('scroll', function() {
-
-
-        scroll_top = $(this).scrollTop();
-        if(scroll_top < last_scroll_top) {
-
-            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
-        }
-        else {
-
-            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
-        }
-        last_scroll_top = scroll_top;
-    });
-}
+$(window).on('resize load', function() {
+  $('body').css({
+    "padding-top": $(".navbar").height()+45 + "px"
+  });
+});
