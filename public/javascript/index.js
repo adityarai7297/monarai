5e93803dac69972c549e34d98c4d8f8ed843996e
Aditya Rai <adityarai@Adityas-MacBook-Pro.local>
2020-10-10 14:48:49 +0530

    fixing navbar

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,7 +1,5 @@
 
-$('body').css({
- "padding-top": $(".navbar").height() + "px"
-});
+
 window.onload = function() {
   var navbar = document.getElementById("navbar");
   navbar.classList.add("sticky")
